7ff776dbaed3cf61908cae88b564bc86f59c4718
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2021-07-10 17:49:42 -0300

    refactor: remove getAllFavorites function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const $starButton = document.querySelector("#star-button");
 
 $starButton.addEventListener("click", addNewFavorite);
-window.addEventListener("load", getAllFavorites);
+window.addEventListener("load", renderFavorites);
 
 async function getTabData() {
   const queryOptions = { active: true, currentWindow: true };
@@ -26,44 +26,41 @@ function addNewFavorite() {
     });
 }
 
-function getAllFavorites() {
+function renderFavorites() {
   chrome.storage.sync.get("STORAGE_KEY", (result) => {
     if(!result["STORAGE_KEY"]) return;
 
+    const $content = document.querySelector("#favorites-link-list");
+    $content.textContent = "";
+
     const favoritesList = JSON.parse(result["STORAGE_KEY"]);
-    renderFavorites(favoritesList);
-  });
-}
 
-function renderFavorites(favoritesList) {
-  const $content = document.querySelector("#favorites-link-list");
-  $content.textContent = "";
+    favoritesList.forEach(({ title, url, favIconUrl }) => {
+      const div = document.createElement("div");
+      div.classList.add("favorite-link-item");
 
-  favoritesList.forEach(({ title, url, favIconUrl }) => {
-    const template = /*html*/ `
-      <div class="favorite-header">
-        <img src="${favIconUrl}" class="favorite-img">
-      </div>
+      const $template = /*html*/ `
+        <div class="favorite-header">
+          <img src="${favIconUrl}" class="favorite-img">
+        </div>
 
-      <div class="favorite-content-container">
-        <div class="favorite-content">
-          <div class="favorite-title">
-            ${title}
-          </div>
+        <div class="favorite-content-container">
+          <div class="favorite-content">
+            <div class="favorite-title">
+              ${title}
+            </div>
 
-          <div class="favorite-icon">
-            <a class="favorite-url" href="${url}" target="_blank">
-              <i class="fas fa-external-link-alt"></i>
-            </a>
-          </div>
-        </div> 
-      </div>
-    `;
+            <div class="favorite-icon">
+              <a class="favorite-url" href="${url}" target="_blank">
+                <i class="fas fa-external-link-alt"></i>
+              </a>
+            </div>
+          </div> 
+        </div>
+      `;
 
-    const div = document.createElement("div");
-    div.classList.add("favorite-link-item");
-    div.insertAdjacentHTML("beforeend", template);
-
-    $content.appendChild(div);
+      div.insertAdjacentHTML("beforeend", $template);
+      $content.appendChild(div);
+    });
   });
 }
